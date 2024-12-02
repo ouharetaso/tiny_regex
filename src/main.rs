091b43fded603af656a6cbe5ce6679c3ce00d06f
ouharetaso <orangepi.hazimekara@gmail.com>
2024-12-02 14:43:53 +0900

    little change in usage text

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use tiny_regex::TinyRegex;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = std::env::args().collect::<Vec<String>>();
-    let usage = format!("Usage: {} [regex_str]\nafter the 1st arguments are ignored", args.get(0).unwrap());
+    let usage = format!("Usage: {} [regex_str]\narguments after the first one are ignored", args.get(0).unwrap());
 
     let regex_str = args.get(1).ok_or_else(|| {eprintln!("{}", usage); "regex string is not provided"})?;
     let re = TinyRegex::new(regex_str).unwrap();
